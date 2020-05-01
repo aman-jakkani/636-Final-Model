2deca95c8a069e5573b576e5e13d2f57a343c287
Aman Jakkani <aj280598@gmail.com>
2020-05-01 16:10:48 -0500

    updating old files

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Replace the variables with your github repo url, repo name, test video name, json named by your UIN
-GIT_REPO_URL="https://github.com/aman-jakkani/bed-recognition-nn.git"
-REPO="bed-recognition-nn/one_video_test"
+GIT_REPO_URL="https://github.com/aman-jakkani/636-Final-Model.git"
+REPO="636-Final-Model/one-video-test"
 VIDEO="test1.mp4"
 UIN_JSON="224007215.json"
 UIN_JPG="224007215.jpg"
